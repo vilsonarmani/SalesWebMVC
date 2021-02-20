5081792dda200fc5da54f83044a55311dd4df46a
vilsonpereira <vilsonpereira@nasajon.com.br>
2021-02-20 18:16:04 -0300

    Chave estrangeira não nula (integridade referencial)

--- SalesWebMVC/Services/SellerService.cs
+++ SalesWebMVC/Services/SellerService.cs
@@ -8,22 +8,23 @@ namespace SalesWebMvc.Services
 {
     public class SellerService 
     {
-        private readonly SalesWebMvcContext _Context;
+        private readonly SalesWebMvcContext _context;
 
         public SellerService(SalesWebMvcContext context)
         {
-            _Context = context;
+            _context = context;
         }
 
         public List<Seller> FindAll()
         {
-            return _Context.Seller.ToList();
+            return _context.Seller.ToList();
         }
 
         public void Insert(Seller obj)
         {
-            _Context.Add(obj);
-            _Context.SaveChanges();
+            obj.Department = _context.Department.First();
+            _context.Add(obj);
+            _context.SaveChanges();
         }
     }
 }
