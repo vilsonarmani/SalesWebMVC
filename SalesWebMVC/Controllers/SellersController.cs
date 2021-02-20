b8c9007c821db5acb3d61120652cadf2944edd86
vilsonpereira <vilsonpereira@nasajon.com.br>
2021-02-20 17:35:56 -0300

    Simple Create Form

--- SalesWebMVC/Controllers/SellersController.cs
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
 using SalesWebMvc.Services;
 
 namespace SalesWebMvc.Controllers
@@ -22,5 +23,21 @@ namespace SalesWebMvc.Controllers
             
             return View(list);//gera um iactionResult contendo a lista; e encaminha para a view
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller )
+        {
+            _sellerService.Insert(seller);
+
+
+            return RedirectToAction(nameof(Index));
+        }
+
     }
 }
---
+++ SalesWebMVC/Services/SellerService.cs
@@ -0,0 +1,29 @@
+﻿using SalesWebMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Services
+{
+    public class SellerService 
+    {
+        private readonly SalesWebMvcContext _Context;
+
+        public SellerService(SalesWebMvcContext context)
+        {
+            _Context = context;
+        }
+
+        public List<Seller> FindAll()
+        {
+            return _Context.Seller.ToList();
+        }
+
+        public void Insert(Seller obj)
+        {
+            _Context.Add(obj);
+            _Context.SaveChanges();
+        }
+    }
+}
